1b08204334ef358d14bea0c2f01ba95d1019f827
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-21 17:34:32 -0400

    Finish Part 3 – How to implement CSS in Reactjs app.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,28 @@
-import React from "react"
+import React from "react";
+import styles from "./TodoItem.module.css";
 
-function TodoItem(props) {
-  return (
-    <li>
-      <input
-        type="checkbox" 
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
-      />
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-        Delete
-      </button>
-      {props.todo.title}
-    </li>
-  )
+class TodoItem extends React.Component {
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    const { completed, id, title } = this.props.todo;
+    return (
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </li>
+    )
+  }
 }
 
 export default TodoItem

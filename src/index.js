92a12b796776f3aae025e681dd90349d850cb4fd
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-21 20:59:44 -0400

    Finish Part 9 – Routing with React Router.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import TodoContainer from "./functionBased/components/TodoContainer"
 //stylesheet
 import "./functionBased/App.css"
 
+import { BrowserRouter as Router } from "react-router-dom"
+
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
